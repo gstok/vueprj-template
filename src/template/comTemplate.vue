7c5ca7cd4270b9a6c5b753a4a93b3fba25258562
鸡毛 <shihao.gu@perfma.com>
2018-09-09 11:51:21 +0800

    完善组件模板

---
+++ src/template/comTemplate.vue
@@ -0,0 +1,72 @@
+
+<!--局部样式-->
+<style scoped>
+
+</style>
+
+<!--局部覆盖样式-->
+<style>
+
+</style>
+
+<template>
+    <div class="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "",
+        props: {
+
+        },
+        data () {
+            return {
+                //#region 页面绑定数据
+                //#endregion
+
+                //#region 页面绑定样式
+                //#endregion
+            };
+        },
+        watch: {
+
+        },
+        computed: {
+            //#region 自动样式计算属性
+            //#endregion
+
+            //#region 常量计算属性
+            //#endregion
+        },
+        methods: {
+            //#region 页面事件方法
+            //#endregion
+
+            //#region 业务逻辑方法
+            //#endregion
+
+            //#region 接口访问方法
+            //#endregion
+
+            //#region 数据转换方法
+            //#endregion
+
+            //#region 自动样式方法
+            //#endregion
+
+            //#region 其他方法
+            //#endregion
+        },
+        created () {
+
+        },
+        mounted () {
+
+        },
+        components: {
+
+        },
+    };
+</script>
+
