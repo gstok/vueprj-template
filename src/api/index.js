3950bc27767b8a527c6599e3ddfb4115433d056d
鸡毛 <shihao.gu@perfma.com>
2018-09-26 15:04:21 +0800

    提交

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 
 //接口分为六类
-//1.获取对象接口，获取一个对象 O 前缀 get
-//2.获取列表接口，获取一个列表 L 前缀 get
-//3.获取分页接口，获取一个分页对象（包含列表） P 前缀 get
-//4.操作行为接口，获取操作行为的结果状态 A 前缀 act
-//5.高级操作行为接口，获取操作行为的结果状态和附加数据 AO 前缀 act
+//1.获取对象接口，获取一个对象 O 前缀 get object
+//2.获取列表接口，获取一个列表 L 前缀 get list
+//3.获取分页接口，获取一个分页对象（包含列表） P 前缀 get page
+//4.操作行为接口，获取操作行为的结果状态 A 前缀 act 
+//5.高级操作行为接口，获取操作行为的结果状态和附加数据 AO 前缀 act data
 //6.其他特殊接口，往往是资源访问接口，比如上传下载文件接口 R 前缀 res
 
 //命名规范：getStudentListL001 前缀 + 自定义命名 + 类型标识 + 接口编号
@@ -15,13 +15,25 @@
 import RTV from "../common/rtv";
 
 
-//获取列表接口
-async function api_getSomeList001 (params) {
+async function api_getObject001 (params) {
     let reqUrl = "";
     let response = await this.$post(reqUrl, params);
     if (response.status == RTV.SUCCESS ||
         response.status == RTV.WARNING) {
-        return response.list;
+        return response.object;
+    }
+    else {
+        return null;
+    }
+}
+
+//获取列表接口
+async function api_getSomeList001 (params) {
+    let reqUrl = "";
+    let rtv = await this.$post(reqUrl, params);
+    if (rtv.status == RTV.SUCCESS ||
+        rtv.status == RTV.WARNING) {
+        return rtv.data.list;
     }
     else {
         return [];
---
+++ src/utils/http.js
@@ -0,0 +1,151 @@
+
+import axios from "axios";
+import { Message } from "element-ui";
+
+let qs = require("qs");
+
+axios.defaults.timeout = 50000;
+axios.defaults.baseURL = '';
+
+//显示错误信息
+function showError (msg) {
+    Message({
+        type: "error",
+        message: msg,
+    });
+}
+
+//显示警报信息
+function showWarning (msg) {
+    Message({
+        type: "warning",
+        message: msg,
+    });
+}
+
+function http (config) {
+    return new Promise((resolve, reject) => {
+        axios(config).then(response => {
+
+            // console.log(response);
+
+            if (response.status === 401) {
+                // 需要跳转
+            }
+            else if (response.status === 403) {
+                // iView.Message.error('权限不足，禁止访问！');
+            }
+            else if (response.status === 500) {
+
+            }
+            else if (response.status !== 200) {
+                // iView.Message.error(response.data.msg);
+            }
+
+            if (response.status != 200) {
+                let message = response.data.message;
+                if (message) {
+                    showError(message);
+                }
+            }
+            else {
+                if (!(response.data.success)) {
+                    let message = response.data.message;
+                    if (message) {
+                        showError(message);
+                    }
+                }    
+            }
+
+            resolve(response.data);
+        }).catch(err => {
+            showError("网络请求发生错误");
+            reject(err);
+        });
+    });
+}
+
+/**
+ * 封装get方法
+ * @param url 要请求的url地址
+ * @param params 请求的参数
+ * @returns {Promise}
+ */
+export function fetch(url, params = { }) {
+    return http({
+        method: 'GET',
+        url: url,
+        params: params,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+/**
+ * 封装post请求
+ * @param url 要请求的url地址
+ * @param data 请求的参数
+ * @returns {Promise}
+ */
+export function post(url, data = { }) {
+    return http({
+        method: 'POST',
+        url: url,
+        data: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+/**
+ * 封装Post提交表单
+ * @param url 要请求的url地址
+ * @param formData 需要提交的表单
+ * @returns {Promise}
+ */
+export function formSub(url, formData = { }) {
+    return http({
+        method: 'POST',
+        url: url,
+        data: formData,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+}
+
+/**
+ * 封装patch请求
+ * @param url 要请求的url地址
+ * @param data 请求的参数
+ * @returns {Promise}
+ */
+export function patch(url, data = { }) {
+    return http({
+        method: 'PATCH',
+        url: url,
+        data: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+/**
+ * 封装put请求
+ * @param url 要请求的url地址
+ * @param data 请求的参数
+ * @returns {Promise}
+ */
+export function put(url, data = { }) {
+    return http({
+        method: 'PUT',
+        url: url,
+        data: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
