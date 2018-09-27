98b2da539e0e1bec651129e974e06657347b87c6
鸡毛 <shihao.gu@perfma.com>
2018-09-27 21:23:26 +0800

    提交

--- gauto/imt/imt.js
+++ gauto/imt/imt.js
@@ -8,6 +8,12 @@ const Moment = require("moment");
 const colors = require("colors");
 
 
+//json数据库文件地址
+let jsonPath = "./api.json";
+//输出Api导出文件地址
+let apiPath = "../../src/api/index.js";
+
+
 //生成函数名
 function funcName (func) {
     let type = func.type.toUpperCase();
@@ -50,8 +56,6 @@ async function api_${ funcName(func) } (params) {
 `;
     return code;
 }
-
-
 //生成顶部引用代码
 function topCode () {
     let code = `
@@ -87,146 +91,134 @@ ${ funcList.map(func => {
 `;
     return code;
 }
-
-
-
-
-
-//从json文件中获取函数对象列表
-function getFuncListFromFile (filePath) {
-    let jsonStr = fs.readFileSync(filePath);
-    return JSON.parse(jsonStr);
-}
-
-//根据函数对象列表生成API导出代码写入到文件中
-function writeApiListToJsFile (funcList, filePath) {
+//生成整个导出文件的代码
+function exportCode (funcList) {
     let code = `
 ${ topCode(funcList) }
 ${ middleCode(funcList) }
 ${ bottomCode(funcList) }
 `;
-    fs.writeFileSync(filePath, code);
     return code;
 }
 
-//显示所有接口列表
-function showAllApi (funcList) {
-    let list = funcList.map(func => {
-        return {
-            id: S(func.id.toString()).padLeft(4, "0").toString(),
-            name: funcName(func),
-            type: func.type,
-            method: func.method,
+
+//#region 功能方法
+    //显示所有接口列表
+    function showAllApi (funcList) {
+        let list = funcList.map(func => {
+            return {
+                id: S(func.id.toString()).padLeft(4, "0").toString(),
+                name: funcName(func),
+                type: func.type,
+                method: func.method,
+            };
+        });
+        list.forEach(item => {
+            console.log(`${ item.id.red }    ${ item.name.green }    ${ item.type.blue }    ${ item.method.yellow }`);
+        });
+    }
+    //根据Id查询接口详情
+    function showApiDetailById (funcList, id) {
+        let func = funcList.find(func => func.id == id);
+        if (func) {
+            let id = S(func.id.toString()).padLeft(4, "0").toString();
+            let name = funcName(func);
+            let type = func.type.toUpperCase();
+            let method = func.method.toLowerCase();
+            console.log(`${ "url:".blue }    ${ func.url.green }`);
+            console.log(`${ id.red }    ${ name.green }    ${ type.blue }    ${ method.yellow }    ${ "warningPass:".blue + func.warningPass.toString().green }`);
+            console.log(funcBuilder(func).trim());
+        }
+        else {
+            console.log("没有找到此接口");
+        }
+    }
+    //根据关键字查询接口列表
+    function showSearchApiList (funcList, keyword) {
+        keyword = keyword.toLowerCase().trim();
+        let searchList = funcList.filter(func => {
+            let name = funcName(func).toLowerCase().trim();
+            return name.indexOf(keyword) > -1;
+        });
+        showAllApi(searchList);
+    }
+    //新增一个接口到数据库，并更新接口导出文件
+    async function addNewApi (funcList) {
+        let idList = funcList.map(func => func.id);
+        let max = Math.max(...idList);
+        let id = max + 1;
+        console.log("请在下方输入接口的基本信息".green);
+        let type = "";
+        while (type != "O" &&
+            type != "L" &&
+            type != "P" &&
+            type != "A" &&
+            type != "AO" &&
+            type != "R") {
+            type = await readLine("type(O,L,P,A,AO,R)：".blue);
+            type = type.toUpperCase().trim();
+        }
+        let name = "";
+        while (!name) {
+            name = await readLine("请输入接口名称：".blue);
+        }
+        let url = "";
+        while (!url) {
+            url = await readLine("请输入接口地址：".blue);
+        }
+        let method = "";
+        while (!method) {
+            method = await readLine("请输入请求方法：".blue);
+            method = method.toLowerCase().trim();
+        }
+        let warningPass = true;
+        let warningPassText = "warningPass";
+        while (warningPassText != "y" &&
+            warningPassText != "n" &&
+            warningPassText != "") {
+            warningPassText = await readLine("警告通过(Y/n)：".blue);
+            warningPassText = warningPassText.toLowerCase().trim();
+        }
+        if (warningPassText == "n") {
+            warningPass = false;
+        }
+        else {
+            warningPass = true;
+        }
+        let remark = await readLine("注释：".blue);
+        let funcObj = {
+            id: id,
+            type: type,
+            name: name,
+            url: url,
+            method: method,
+            warningPass: warningPass,
+            remark: remark,
         };
-    });
-    list.forEach(item => {
-        console.log(`${ item.id.red }    ${ item.name.green }    ${ item.type.blue }    ${ item.method.yellow }`);
-    });
-}
-
-//根据Id查询接口详情
-function showApiDetailById (funcList, id) {
-    let func = funcList.find(func => func.id == id);
-    if (func) {
-        let id = S(func.id.toString()).padLeft(4, "0").toString();
-        let name = funcName(func);
-        let type = func.type.toUpperCase();
-        let method = func.method.toLowerCase();
-        console.log(`${ "url:".blue }    ${ func.url.green }`);
-        console.log(`${ id.red }    ${ name.green }    ${ type.blue }    ${ method.yellow }    ${ "warningPass:".blue + func.warningPass.toString().green }`);
-        console.log(funcBuilder(func).trim());
+        funcList.push(funcObj);
+        writeFuncList(funcList, jsonPath);
+        writeApiExportFile(funcList, apiPath);
     }
-    else {
-        console.log("没有找到此接口");
+//#endregion
+
+//#region 文件IO方法
+    //从JSON文件之中读取函数对象列表
+    function readFuncList (filePath) {
+        let jsonStr = fs.readFileSync(filePath);
+        return JSON.parse(jsonStr);
     }
-}
-
-//根据关键字查询接口列表
-function showSearchApiList (funcList, keyword) {
-    keyword = keyword.toLowerCase().trim();
-    let searchList = funcList.filter(func => {
-        let name = funcName(func).toLowerCase().trim();
-        return name.indexOf(keyword) > -1;
-    });
-    showAllApi(searchList);
-}
-
-async function addNewApi (funcList, jsonPath, jsPath) {
-    let idList = funcList.map(func => func.id);
-    let max = Math.max(...idList);
-    let id = max + 1;
-    console.log("请在下方输入接口的基本信息".green);
-    let type = "";
-    while (type != "O" &&
-           type != "L" &&
-           type != "P" &&
-           type != "A" &&
-           type != "AO" &&
-           type != "R") {
-        type = await readLine("type(O,L,P,A,AO,R)：".blue);
-        type = type.toUpperCase().trim();
+    //向JSON文件之中写入函数对象列表
+    function writeFuncList (funcList, filePath) {
+        let jsonStr = JSON.stringify(funcList, null, 4);
+        fs.writeFileSync(filePath, jsonStr);
     }
-    let name = "";
-    while (!name) {
-        name = await readLine("请输入接口名称：".blue);
+    //根据函数对象列表写API导出文件
+    function writeApiExportFile (funcList, filePath) {
+        let code = exportCode(funcList);
+        fs.writeFileSync(filePath, code);
     }
-    let url = "";
-    while (!url) {
-        url = await readLine("请输入接口地址：".blue);
-    }
-    let method = "";
-    while (!method) {
-        method = await readLine("请输入请求方法：".blue);
-        method = method.toLowerCase().trim();
-    }
-    let warningPass = true;
-    let warningPassText = "warningPass";
-    while (warningPassText != "y" &&
-           warningPassText != "n" &&
-           warningPassText != "") {
-        warningPassText = await readLine("警告通过(Y/n)：".blue);
-        warningPassText = warningPassText.toLowerCase().trim();
-    }
-    if (warningPassText == "n") {
-        warningPass = false;
-    }
-    else {
-        warningPass = true;
-    }
-    let remark = await readLine("注释：".blue);
-    let funcObj = {
-        id: id,
-        type: type,
-        name: name,
-        url: url,
-        method: method,
-        warningPass: warningPass,
-        remark: remark,
-    };
-    funcList.push(funcObj);
-    buildJsonFile(funcList, jsonPath);
-    buildApiFile(jsonPath, jsPath);
-}
+//#endregion
 
-
-//生成Json文件
-function buildJsonFile (funcList, jsonPath) {
-    let jsonStr = JSON.stringify(funcList, null, 4);
-    fs.writeFileSync(jsonPath, jsonStr);
-}
-
-//生成接口文件
-function buildApiFile (jsonPath, jsPath) {
-    let funcList = getFuncListFromFile(jsonPath);
-    writeApiListToJsFile(funcList, jsPath);
-}
-
-
-
-
-// buildApiFile("./api.json", "../../src/api/index.js");
-// console.log("接口导出代码生成成功！");
-// process.exit();
 
 async function readLine (ques) {
     return new Promise((resolve, reject) => {
@@ -248,9 +240,14 @@ readLineSys.on("close", () => {
     console.log("谢谢使用".green);
 });
 
-(async () => {
-    let funcList = [];
-    funcList = getFuncListFromFile("./api.json");
+
+
+
+
+
+//主函数
+async function main () {
+    let funcList = readFuncList(jsonPath);
     console.log("欢迎使用鸡毛接口管理工具！".green);
     while (true) {
         let text = await readLineCmd();
@@ -273,4 +270,6 @@ readLineSys.on("close", () => {
         }
     }
     readLineSys.close();
-})();
+};
+
+main();
--- src/api/index.js
+++ src/api/index.js
@@ -1,103 +1,33 @@
 
 
-//自动生成的接口导入代码 生成时间 2018-09-27 20:57:28
+//自动生成的接口导入代码 生成时间 2018-09-27 21:21:13
 import Http from "@/utils/http";
 import respChanger from "@/common/respChanger";
 
 
 
-//暂未填写接口注释
-async function api_getGetPlanMetaByIdO0001 (params) {
-    let reqUrl = "/api/xsea/plan/getPlanMetaById";
+//获得一个学生的基本信息
+async function api_getGetStudentO0001 (params) {
+    let reqUrl = "/api/student/get";
     let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "O", false);
-}
-
-
-//暂未填写接口注释
-async function api_getGetPlanSceneListL0002 (params) {
-    let reqUrl = "/api/xsea/plan/queryPlanSceneList";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "L", true);
-}
-
-
-//暂未填写接口注释
-async function api_getGetPlanPageP0003 (params) {
-    let reqUrl = "/api/xsea/plan/list";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "P", true);
-}
-
-
-//暂未填写接口注释
-async function api_actStopTestA0004 (params) {
-    let reqUrl = "/api/xsea/sceneExec/stop";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "A", true);
-}
-
-
-//暂未填写接口注释
-async function api_actSaveSceneMetaAO0005 (params) {
-    let reqUrl = "/api/xsea/scene/saveSceneMeta";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "AO", true);
-}
-
-
-//保存
-async function api_actSaveAO0006 (params) {
-    let reqUrl = "/api/save";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "AO", true);
+    return respChanger(respRtv, "O", true);
 }
 
 
 //删除
-async function api_actDeleteA0007 (params) {
+async function api_actDeleteA0002 (params) {
     let reqUrl = "/api/delete";
     let respRtv = await Http.post(reqUrl, params);
     return respChanger(respRtv, "A", true);
 }
 
 
-//更新数据
-async function api_actUpdateA0008 (params) {
-    let reqUrl = "/api/update";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "A", true);
-}
 
-
-//哈哈
-async function api_getHahaO0009 (params) {
-    let reqUrl = "/api/gethaha";
-    let respRtv = await Http.post(reqUrl, params);
-    return respChanger(respRtv, "O", true);
-}
-
-
-
-export const getGetPlanMetaByIdO0001 = api_getGetPlanMetaByIdO0001;
-export const getGetPlanSceneListL0002 = api_getGetPlanSceneListL0002;
-export const getGetPlanPageP0003 = api_getGetPlanPageP0003;
-export const actStopTestA0004 = api_actStopTestA0004;
-export const actSaveSceneMetaAO0005 = api_actSaveSceneMetaAO0005;
-export const actSaveAO0006 = api_actSaveAO0006;
-export const actDeleteA0007 = api_actDeleteA0007;
-export const actUpdateA0008 = api_actUpdateA0008;
-export const getHahaO0009 = api_getHahaO0009;
+export const getGetStudentO0001 = api_getGetStudentO0001;
+export const actDeleteA0002 = api_actDeleteA0002;
 
 export default {
-    getGetPlanMetaByIdO0001: api_getGetPlanMetaByIdO0001,
-    getGetPlanSceneListL0002: api_getGetPlanSceneListL0002,
-    getGetPlanPageP0003: api_getGetPlanPageP0003,
-    actStopTestA0004: api_actStopTestA0004,
-    actSaveSceneMetaAO0005: api_actSaveSceneMetaAO0005,
-    actSaveAO0006: api_actSaveAO0006,
-    actDeleteA0007: api_actDeleteA0007,
-    actUpdateA0008: api_actUpdateA0008,
-    getHahaO0009: api_getHahaO0009,
+    getGetStudentO0001: api_getGetStudentO0001,
+    actDeleteA0002: api_actDeleteA0002,
 };
 
