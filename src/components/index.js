b7061c6469b32e9360609b4586cc039526380d71
鸡毛 <shihao.gu@perfma.com>
2018-09-29 19:39:08 +0800

    修改提交

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,17 @@
 
-import vue_ccStrategyLine from "@/components/cCom/strategyLine.vue";
-import vue_fcNodeEditWindow from "@/components/fCom/nodeEditWindow.vue";
-import vue_uiButton from "@/components/uiCom/button.vue";
+import vue_StrategyLine from "../@/components/cCom/strategyLine.vue";
+import vue_NodeEditWindow from "../@/components/fCom/nodeEditWindow.vue";
+import vue_Button from "../@/components/uiCom/button.vue";
 
-export const ccStrategyLine = { install: Vue => Vue.component("ccStrategyLine", vue_ccStrategyLine) };
-export const fcNodeEditWindow = { install: Vue => Vue.component("fcNodeEditWindow", vue_fcNodeEditWindow) };
-export const uiButton = { install: Vue => Vue.component("uiButton", vue_uiButton) };
+export const StrategyLine = { install: Vue => Vue.component("StrategyLine", vue_StrategyLine) };
+export const NodeEditWindow = { install: Vue => Vue.component("NodeEditWindow", vue_NodeEditWindow) };
+export const Button = { install: Vue => Vue.component("Button", vue_Button) };
 
 const components = {
     install: Vue => {
-        Vue.component("ccStrategyLine", vue_ccStrategyLine);
-        Vue.component("fcNodeEditWindow", vue_fcNodeEditWindow);
-        Vue.component("uiButton", vue_uiButton);
+        Vue.component("StrategyLine", vue_StrategyLine);
+        Vue.component("NodeEditWindow", vue_NodeEditWindow);
+        Vue.component("Button", vue_Button);
     }
 };
 
