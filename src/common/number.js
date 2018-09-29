23738b5332730a0618ddd04e1a097e8921111bdc
鸡毛 <shihao.gu@perfma.com>
2018-09-29 20:01:38 +0800

    取精度函数设计中

---
+++ src/common/number.js
@@ -0,0 +1,11 @@
+
+//按精度取一个小数
+function accuracy (decimal, accu) {
+    if (accu > -1) {
+        let numStr = decimal.toString();
+        let strSeg = numStr.split(".");
+    }
+    else {
+        return decimal;
+    }
+}
